bd3bd62e0c91feacdd063cf9c2b53eb6715a7af6
Belosnejka <153391631+Belosnejechka@users.noreply.github.com>
2024-04-22 11:37:40 +0300

    fix

--- src/main/java/netology/Main.java
+++ src/main/java/netology/Main.java
@@ -1,25 +1,23 @@
 package netology;
 
-import org.apache.http.HttpHeaders;
-import org.apache.http.ParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.parser.JSONParser;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
+import javax.imageio.ImageIO;
 import java.io.*;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class Main {
+    public static final ObjectMapper objectMapper = new ObjectMapper();
+
     public static final String REMOTE_SERVICE_URI = "https://api.nasa.gov/planetary/apod?api_key=xyHFK4MxE4enxO5wrwqxyXOdFuSI2ViZtSdTuOee";
 
     public static void main(String[] args) throws IOException {
+
+
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
@@ -29,38 +27,20 @@ public class Main {
                 .build();
 
         HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
-        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
         CloseableHttpResponse response = httpClient.execute(request);
-        String JSON = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
-        JSONParser parser = new JSONParser();
-        Nasa nasa1 = null;
-        try {
-            Object nasa = parser.parse(JSON);
-            GsonBuilder gsonBuilder = new GsonBuilder();
-            Gson gson = gsonBuilder.create();
-            nasa1 = gson.fromJson(String.valueOf(nasa), Nasa.class);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (org.json.simple.parser.ParseException e) {
-            throw new RuntimeException(e);
-        }
 
-        HttpGet request2 = new HttpGet(nasa1.getUrl());
-        request2.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
-        CloseableHttpResponse response2 = httpClient.execute(request2);
-        String JSON2 = new String(response2.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
-        String filename = nasa1.getUrl().substring(nasa1.getUrl().lastIndexOf('/') + 1) + ".html";
-        File myFile = new File("/Users/belosnejka/IdeaProjects/Projects/netology/Work/HomeWork4.9.2/src/main/resources/" + filename);
-        try {
-            if (myFile.createNewFile())
-                System.out.println("Файл был создан");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(myFile))) {
-            bw.write(JSON2);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        //json в java-объект
+        Nasa nasa = objectMapper.readValue(response.getEntity().getContent(), Nasa.class);
+        String url = nasa.getUrl();
+
+        //новый http-запрос
+        HttpGet requestNew = new HttpGet(url);
+        CloseableHttpResponse responseNew = httpClient.execute(requestNew);
+        String[] arrayUrlImage = url.split("/");
+
+        //сохраняем ответ в файл с именем, извлеченным из url
+        File file = new File(arrayUrlImage[arrayUrlImage.length - 1]);
+        ImageIO.write(ImageIO.read(responseNew.getEntity().getContent()), "jpg", file);
+
     }
 }
--- src/main/java/netology/Nasa.java
+++ src/main/java/netology/Nasa.java
@@ -1,13 +1,34 @@
 package netology;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Nasa {
-    String date; //"2024-04-21",
-    String explanation; //"Watch Juno zoom past Jupiter.  NASA's robotic spacecraft Juno is continuing on its now month-long, highly-elongated orbits around our Solar System's largest planet.  The featured video is from perijove 16, the sixteenth time that Juno passed near Jupiter since it arrived in mid-2016. Each perijove passes near a slightly different part of Jupiter's cloud tops.  This color-enhanced video has been digitally composed from 21 JunoCam still images, resulting in a 125-fold time-lapse. The video begins with Jupiter rising as Juno approaches from the north. As Juno reaches its closest view -- from about 3,500 kilometers over Jupiter's cloud tops -- the spacecraft captures the great planet in tremendous detail. Juno passes light zones and dark belts of clouds that circle the planet, as well as numerous swirling circular storms, many of which are larger than hurricanes on Earth.  As Juno moves away, the remarkable dolphin-shaped cloud is visible.  After the perijove, Jupiter recedes into the distance, now displaying the unusual clouds that appear over Jupiter's south.  To get desired science data, Juno swoops so close to Jupiter that its instruments are exposed to very high levels of radiation.",
-    String media_type; //"video",
-    String service_version; //"v1",
-    String title; //"Perijove 16: Passing Jupiter",
-    String url; //"https://www.youtube.com/embed/c4TU3arrZR8?rel=0"
+    private final String copyright;
+    private final String date;
+    private final String explanation;
+    private final String hdurl;
+    private final String media_type;
+    private final String service_version;
+    private final String title;
+    private final String url;
+
+    public Nasa(@JsonProperty("copyright") String copyright,
+                @JsonProperty("date") String date,
+                @JsonProperty("explanation") String explanation,
+                @JsonProperty("hdurl") String hdurl,
+                @JsonProperty("media_type") String media_type,
+                @JsonProperty("service_version") String service_version,
+                @JsonProperty("title") String title,
+                @JsonProperty("url") String url) {
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
+    }
 }
